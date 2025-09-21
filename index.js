7fe0f78357fb62254b218de16d4f3b83e0f3c108
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-09-21 12:40:16 +0500

    refactor: add swagger doc

--- index.js
+++ index.js
@@ -2,15 +2,39 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+
 import playerRoutes from './routes/player.js';
 
 const app = express();
 const port = 3000;
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "NBA Players API",
+      version: "1.0.0",
+      description: "A simple API to manage NBA players",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000", 
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 app.use('/api', playerRoutes);
 
@@ -20,4 +44,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Swagger docs at http://localhost:${port}/api-docs`);
 });
